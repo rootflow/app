f52fe792f8efd445334305f9237f64dc4aa629e3
Sergey Sova <i.am@lestad.net>
2016-12-08 04:25:24 +0300

    Add pug and some views

--- server/index.js
+++ server/index.js
@@ -1,17 +1,24 @@
 #!/usr/bin/env node
 
+const { resolve } = require('path')
 const Express = require('express')
-const { urlencoded } = require('body-parser')
 const cors = require('cors')
 const morgan = require('morgan')
+const { urlencoded } = require('body-parser')
 
 const routes = require('./routes')
 
+// constants
 const PORT = process.env.PORT || 3000
 global.DEVELOPMENT = process.env.NODE_ENV !== 'production'
 
 const app = Express()
 
+// settings
+app.set('view engine', 'pug')
+app.set('views', resolve(__dirname, 'views'))
+
+// middlewares
 app.use(cors())
 app.use(urlencoded({ extended: true }))
 app.use(morgan(DEVELOPMENT ? 'dev' : 'common'))
--- server/routes/home.js
+++ server/routes/home.js
@@ -3,9 +3,9 @@ const { Router } = require('express')
 const router = module.exports = Router()
 
 router.get('/', (req, res) => {
-  res.status(200).send('ok')
+  res.render('home/index')
 })
 
 router.get('/about', (req, res) => {
-  res.status(200).send('about')
+  res.render('home/about')
 })
